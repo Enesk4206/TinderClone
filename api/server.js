4982b5031b89dec62bc0937e5f0565f63ad42084
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-11-11 12:37:34 +0300

    1-) Api side Routes initiliazed

---
+++ api/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//routes 
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import matchRoutes from "./routes/matchRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+
+dotenv.config(); //that line could give us to able process.env.PORT use nontrouble
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json()) // it can useable to req.body like (when user send e request information we can get to that information and process)
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/matches", matchRoutes);
+
+
+app.listen(port, ()=>{
+    console.log(`Server starting to the port: http://localhost:${port}`)
+});
